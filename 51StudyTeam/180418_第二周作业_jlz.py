8ec2e62db8d9932e7aecb83f994e7466eb2fe025
周金龙(zhoujinlong) <zhoujinlong@51talk.com>
2018-04-24 10:34:23 +0800

    更新第二周的作业

--- 51StudyTeam/180418_第二周作业_jlz.py
+++ 51StudyTeam/180418_第二周作业_jlz.py
@@ -17,7 +17,7 @@ class Chen():
         7.	求list1的长度
         9.	List1+list2的新list6,输出新list6
         11.	list1的末尾插入’baidu’这个元素
-        14.	删除list1中的四二个值       #没看懂题目是什么意思？？？
+        14.	删除list1中的第二个值      
         15.	将list1倒序输出
         16.	将list2排序
         :return:
@@ -42,6 +42,9 @@ class Chen():
 
         list1.append('baidu')
         print "11.	list1的末尾插入’baidu’这个元素:", list1
+
+        del list1[1]
+        print "14.	删除list1中的第二个值:", list1
 
         list1.reverse()     #list.reverse()和list.sort是在列表内部改变排序，没有返回值
         list2.sort()
@@ -84,6 +87,79 @@ class Chen():
         print "12、123在alist中出现的次数：", aList.count(123)
         print "13.	计算runoob在list中的索引位置:", aList.index('Runoob')
 
+    def seventeen(self):
+        '''
+        17.	dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'},输出name所对应的kye
+        :return: 
+        '''
+        dict = {'Name':'Runoob', 'Age':7, 'Class':'First'}
+        print "17、dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'},输出name所对应的key:",dict['Name']
+
+    def eighteen(self):
+        '''
+        18.	# Fibonacci series: 斐波纳契数列 # 两个元素的总和确定了下一个数,写一个程序计算10以内菲波那切数列,并把结果输入在同一列
+        斐波那契数列：
+        F(0)=1,F(1)=1;
+        F(2)=F(2-1)+F(2-2)=F(1)+F(0)=1+1=2;
+        F(3)=F(2)+F(1)=3;
+        F(4)=4;
+        ...
+        F(n) = F(n-1) + F(n-2)
+        :return: 
+        '''
+        num = input("18、列出0到？的斐波那契数列？")
+        F = [1,1]
+
+        for n in range(2,num+1):
+            f = F[n-1] + F[n-2]
+            if f >= num:
+                break
+            else:
+                F.append(f)
+        print F
+
+    def feb(self):
+        '''
+        慧燕斐波那契数列答案
+        :return: 
+        '''
+        f1, f2 = 0, 1
+        while f2 < 10:
+            print(f2)
+            f1, f2 = f2, f1 + f2
+
+
+    def ninteen(self):
+        '''
+        19.	写一个程序,让用户输入年龄,0-3岁告诉用户是婴儿,3-6岁幼儿,6-12岁少年,12-18青年,18岁以上是成人
+        :return: 
+        '''
+        num = input("请输入年龄，仅限数字：")
+        if num in range(0,3):
+            print "婴儿"
+        elif num in range(3,6):
+            print "幼儿"
+        elif num in range(6,12):
+            print "少年"
+        elif num in range(12,18):
+            print '青年'
+        else:
+            print "成年人"
+
+    def twenty(self):
+        '''
+        20.	计算0-100的整数之和
+        :return: 
+        '''
+        num = input("你想计算0到多少的整数之和：")
+        lists = range(0, num+1)     #需要➕1，不然会少算最后一个数
+        sum = 0
+        # print lists
+        for i in range(0,len(lists)):
+            # print i
+            sum = sum + lists[i]
+        print "0到%d的整数之和是%d" % (num, sum)
+
     def twentyOne(self):
         '''
         21.	计算并输出0-10的质数,提示,质数:只有1和他本身两个因数,只能被1和他本身整除
@@ -117,9 +193,14 @@ class Chen():
             print "\n"
 
 if __name__ == '__main__':
-    Chen().one()
+    # Chen().one()
     # Chen().eight()
     # Chen().ten()    #有问题，没写完
     # Chen().twelve()
+    # Chen().seventeen()
+    # Chen().eighteen()
+    # Chen().ninteen()
+    # Chen().twenty()
     # Chen().twentyOne()
     # Chen().twentyTwo()
+    Chen().feb()
