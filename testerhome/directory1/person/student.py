b830b761cf0bd335bbb7f4be2b9cf2f5cd21144a
cznccsjd <cznccsjd@126.com>
2019-05-17 18:55:15 +0800

    提交之前未及时提交的文件

--- testerhome/directory1/person/student.py
+++ testerhome/directory1/person/student.py
@@ -3,4 +3,43 @@
 from person import Person
 class Student(Person):
 
-    def __init__(self,name, sex, province, grade):
+    def __init__(self, name, sex, province, grade):
+        super(Student,self).__init__(name,sex,province)     #super()方法继承父类
+        # Person.__init__(self,name,sex,province)         #直接用Person类来继承
+        self.grade = grade
+        # self.name = 'i am not %s'% name
+
+    def get_grade(self):
+        return self.grade
+
+    # 静态方法
+    @staticmethod
+    def set_name(newName):
+        print '设置的新名字是',newName
+        Person().newName = newName
+        # return newName
+
+    def get_name(self):
+        """
+        重写父类的方法，实际上就是多态
+        :return:
+        """
+        print "i am class Student"
+        print "Stundet方法中,name的名字",self.name
+
+
+    def get_newName(self):
+        print self.newName
+
+
+
+
+
+
+if __name__ == "__main__":
+    ss = Student("zhangs","male","hunan",3)
+    # print ss.get_grade()
+    # print ss.get_name()
+
+    ss.set_name("hanmeimei")
+    ss.get_newName()
--- testerhome/learnModules/learnLoggingModule.py
+++ testerhome/learnModules/learnLoggingModule.py
@@ -4,9 +4,11 @@
 学习logging模块
 """
 import logging
+from logging.handlers import RotatingFileHandler
+
 def log_basic():
     # CRITICAL 50; ERROR 40; WARNING 30; INFO 20: DEBUG 10; NOSET 0;
-    logging.basicConfig(filename="test.log",level=0,
+    logging.basicConfig(filename="test.log",level=50,
                         format='%(asctime)s %(name)s %(levelname)s %(module)s:%(lineno)d %(message)s')
     logging.critical('this is critical log')
     logging.debug("this is debug log")
@@ -14,6 +16,11 @@ def log_basic():
     logging.warn('this is warn log')
     logging.error('this is error log')
     logging.fatal('this is fatal log')
+    # 定义RotatingFileHandler,日志文件大小超过5KB时进行旋转，最大5个备份日志文件
+    # RotatingFileHandler('back.log',maxBytes=1024*1,mode='a',backupCount=2)
+    #
+    logging.getLogger("yayaay").setLevel(logging.ERROR)
+# def
 
 
 
