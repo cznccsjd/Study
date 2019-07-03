90b4d0d58686c8c4d900b310a39b4978665ada92
jlz <cznccsjd@126.com>
2019-07-03 08:38:32 +0800

    新增：testerhome SE学习文件

---
+++ testerhome/selenium_test190216/temp/test_boolean.py
@@ -0,0 +1,11 @@
+import pytest
+import os
+def exists():
+    
+
+class Boolean():
+    def setup_class(self):
+        pass
+
+    def test_exist_file(self):
+        file_name = os.path.exists(r"D:\Documents\workspace\Study\testerhome\selenium_test190216\temp\__init__.py")
