621e26b19d6d19743a3eca1f1445fe4ff4481ed6
cznccsjd <cznccsjd@126.com>
2019-07-20 10:31:03 +0800

    request新增try..except

--- testForWork/Service/requestAndSoup.py
+++ testForWork/Service/requestAndSoup.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python3
 #coding:utf-8
 
-import requests
+import requests, time
 from bs4 import BeautifulSoup
 
 class RequestAndSoup():
-    def request(self, url, type='get', params=None, data=None, json=None, **kwargs):
+    def request(self, url, type='get', params=None, data=None, json=None, sleeptime=0, **kwargs):
+        # 关闭多余的连接
+        s = requests.sessions.session()
+        s.keep_alive = False
+
         if type == 'post':
-            res = requests.post(url, data, json, **kwargs)
+            try:
+                res = requests.post('post', url, data, json, **kwargs)
+                time.sleep(sleeptime)
+            except Exception as e:
+                print("ERROR:%s" %e)
         else:
-            res = requests.get(url, params, **kwargs)
+            try:
+                res = requests.get(url, params, **kwargs)
+                time.sleep(sleeptime)
+            except Exception as e:
+                print("ERROR:%s" %e)
+        return res
 
 
-        pass
+
 
     def soup(self):
+        RequestAndSoup().request()
         pass
 
 if __name__ == '__main__':
