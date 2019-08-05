19f1d330b4befc2767b270929460b15e52677c4f
cznccsjd <cznccsjd@126.com>
2019-08-05 18:54:24 +0800

    完善登录接口

--- testForWork/Service/CrmLogin.py
+++ testForWork/Service/CrmLogin.py
@@ -5,6 +5,7 @@ crm登录，并返回登录后的cookie
 
 from selenium import webdriver
 from testForWork.Common import DomainUrl
+from testForWork.Common.transdata import Trans
 import requests
 import time
 
@@ -14,18 +15,30 @@ class CrmLogin():
     def login(self):
         url = urlDefault + '/admin/admin_login.php'
         urlLogin = urlDefault + '/admin/login.php'
-        payload = {'user_name':'admin','password':123456}
-        res = requests.get(url)
-        cookie = res.cookies
-        print(cookie)
-        header = {
-            'Cookie':1
+        urlTest = urlDefault + '/Channel/channelList'
+        payload = {
+            'user_name':'admin',
+            'password':123456,
+            'ref':'',
+            'user_type':'admin',
+            'Submit':'登 录'
         }
+        header = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36',
+            'Referer': 'http://crm.51talk.com/admin/admin_login.php',
+        }
+        s = requests.Session()
+        res = s.get(url)
+        cookie = res.cookies.get_dict()
+        resLogin = s.post(urlLogin,data=payload,headers=header)
+        cookieLogin = resLogin.cookies
+        print('这种形式的cookie也可以的，返回给其他端s就行了：', cookieLogin)
+        # 测试cookie是否生效
+        resJihui = s.get(urlTest)
+        if u"机会分类配置" in resJihui.text:
+            print(111)
+            print('后续考虑，返回什么值？')
 
-        resLogin = requests.post(url,data=payload,headers=header)
-        print(resLogin)
-
-        pass
 
     def loginSe(self, uname='admin', upasswd='123456'):
         '''
@@ -51,8 +64,6 @@ class CrmLogin():
         dr.close()
         print('%s登录成功后的cookie：%s' % (uname, cookies))
         return cookies
-
-
 
 if __name__ == '__main__':
     # CrmLogin().loginSe()
