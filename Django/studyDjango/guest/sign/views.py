73faa2a49d10b7466dea697b58cac9f23cf08692
jlz <cznccsjd@126.com>
2019-08-18 22:31:16 +0800

    Django按虫师学习文档，有问题，页面刷不出guest的数据

---
+++ Django/studyDjango/guest/sign/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from sign.models import Event, Guest
+
+# Create your views here.
+def index(request):
+    # return HttpResponse("Hello Django!")
+    return render(request, "index.html")
+
+# 登录动作
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        # if username == 'admin' and password == 'admin123':
+        if user is not None:
+            auth.login(request, user)       #登录
+            # return HttpResponse('login success!')
+            response = HttpResponseRedirect('/event_manage')
+            # response.set_cookie('user', username, 3600)     #添加浏览器cookie
+            request.session['user'] = username      #将Session信息记录到浏览器
+            return response
+        else:
+            return render(request, 'index.html', {'error': 'username or password error!'})
+
+def logout(request):
+    return render(request, "index.html")
+
+# 发布会管理
+@login_required
+def event_manage(request):
+    event_list = Event.objects.all()
+    # username = request.COOKIES.get('user', '')      #读取浏览器cookie
+    username = request.session.get('user', '')      #读取浏览器session
+    return render(request, 'event_manage.html', {"user":username, "events":event_list})
+
+# 嘉宾管理
+def guest_manage(request):
+    username = request.session.get('user', '')
+    guest_list = Guest.objects.all()
+    return render(request, "guest_manage.html", {"user":username, "guest":guest_list})
+
+# 发布会名称搜索
+@login_required
+def search_name(request):
+    username = request.session.get('user', '')
+    search_name = request.GET.get("name", "")
+    event_list = Event.objects.filter(name__contains=search_name)
+    return render(request, "event_manage.html", {"user":username, "events":event_list})
+
+if __name__ == '__main__':
+    guest_manage()
