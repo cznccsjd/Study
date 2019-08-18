dd36ea4edba24ec800feb40533b447ac6271db39
jlz <cznccsjd@126.com>
2019-08-19 00:15:37 +0800

    新增guest页面的分页，但是报错了，需要解决问题

--- Django/studyDjango/guest/sign/views.py
+++ Django/studyDjango/guest/sign/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from sign.models import Event, Guest
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def index(request):
@@ -41,7 +42,17 @@ def event_manage(request):
 def guest_manage(request):
     username = request.session.get('user', '')
     guest_list = Guest.objects.all()
-    return render(request, "guest_manage.html", {"user":username, "guest":guest_list})
+    paginator = Paginator(guest_list, 2)
+    page = request.Get.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # 如果page不是整数，取第一页面数据
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # 如果page不在范围，取最后一页面
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, "guest_manage.html", {"user":username, "guest":contacts})
 
 # 发布会名称搜索
 @login_required
