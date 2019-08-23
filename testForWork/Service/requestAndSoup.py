8bde3fccb2f03b2ff1326ef00ad5aab4f443c14d
cznccsjd <cznccsjd@126.com>
2019-08-23 20:08:14 +0800

    优化post公共方法

--- testForWork/Service/requestAndSoup.py
+++ testForWork/Service/requestAndSoup.py
@@ -5,23 +5,27 @@ import requests, time
 from bs4 import BeautifulSoup
 
 class RequestAndSoup():
-    def request(self, url, type='get', cookies=None, params=None, data=None, json=None, sleeptime=0, **kwargs):
-        # 关闭多余的连接
-        s = requests.sessions.session()
-        s.keep_alive = False
+    def request(self, url, type='get', session=None, params=None, data=None, json=None, sleeptime=0, **kwargs):
+        # 判断是否有Session，如果没有，新建一个
+        if session == None:
+            s = requests.Session()
+        else:
+            s = session
 
         if type == 'post':
             '''
             post方法有问题，需要详细了解下post用法
             '''
             try:
-                resp = requests.post(url, data, cookies, json, **kwargs)
+                # resp = requests.post(url, data, cookies, json, **kwargs)
+                resp = s.post(url, data=None, json=None, **kwargs)
                 time.sleep(sleeptime)
             except Exception as e:
                 print("ERROR:%s" %e)
         else:
             try:
-                resp = requests.get(url, params, cookies, **kwargs)
+                # resp = requests.get(url, params, cookies, **kwargs)
+                resp = s.get(url, **kwargs)
                 time.sleep(sleeptime)
             except Exception as e:
                 print("ERROR:%s" %e)
