84193c3b179eb63bf61252075a0e03a157d45277
jlz <cznccsjd@126.com>
2019-09-03 13:18:52 +0800

    更新：Django测试用例

--- Django/studyDjango/guest/sign/tests.py
+++ Django/studyDjango/guest/sign/tests.py
@@ -76,3 +76,56 @@ class LoginActionTest(TestCase):
         test_data = {'username':'admin', 'password':'admin123456'}
         response = self.client.post('/login_action', data=test_data)
         self.assertEqual(response.status_code, 302)
+
+class EventManageTest(TestCase):
+    '''
+    发布会管理
+    '''
+    def setUp(self):
+        User.objects.create_user('admin', 'admin@mail.com', 'admin123456')
+        Event.objects.create(name='xiaomi5', limit=2000, address='beijing', status=1, start_time='2019-09-05 12:00:00')
+        self.login_user = {'username':'admin', 'password':'admin123456'}
+
+    def test_event_manage_success(self):
+        '''
+        测试发布会：xiaomi5
+        :return:
+        '''
+        response = self.client.post('/login_action/', data=self.login_user)
+        response = self.client.post('/event_manage/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"xiaomi5", response.content)
+        self.assertIn(b"beijing", response.content)
+
+
+class GuestManageTest(TestCase):
+    '''
+    嘉宾管理
+    '''
+    def setUp(self):
+        User.objects.create_user('admin', 'admin@mail.com', 'admin123456')
+        Event.objects.create(id=1, name='xiaomi5', limit=2000, address='beijing', status=1, start_time='2019-09-05 12:00:00')
+        Guest.objects.create(realname='alen', phone=18601000111, email='alen@mail.com', sign=0, event_id=1)
+        self.login_user = {'username':'admin', 'password':'admin123456'}
+
+    def test_event_manage_success(self):
+        '''
+        测试嘉宾信息：alen
+        :return:
+        '''
+        response = self.client.post('/login_action/', data=self.login_user)
+        response = self.client.post('/guest_manage/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'alen', response.content)
+        self.assertIn(b'18611000000', response.content)
+
+    def test_guest_manage_search_success(self):
+        '''
+        测试嘉宾搜索
+        :return:
+        '''
+        response = self.client.post('/login_action/', data = self.login_user)
+        response = self.client.post('/search_phone/', {'phone':'18601000111'})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'alen', response.content)
+        self.assertIn(b'18601000111', response.content)
